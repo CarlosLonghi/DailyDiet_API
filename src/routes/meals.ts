a9562010a11c2358ab81f8bcfe4f429d6654daca
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-02-27 16:10:27 -0300

    feat: create middleware and create meals with userId

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,11 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { checkUserIdExist } from '../middlewares/check-user-id-exist'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
+  app.post('/', { preHandler: [checkUserIdExist] }, async (request, reply) => {
     const createMealsBodySchema = z.object({
-      user_id: z.string(),
       name: z.string(),
       description: z.string(),
       date: z.string(),
@@ -14,13 +14,15 @@ export async function mealsRoutes(app: FastifyInstance) {
       diet_compliant: z.enum(['yes', 'no']),
     })
 
+    const { userId } = request.cookies
+
     // eslint-disable-next-line
-    const { user_id, name, description, date, hour, diet_compliant } =
+    const { name, description, date, hour, diet_compliant } =
       createMealsBodySchema.parse(request.body)
 
     await knex('meals').insert({
       id: randomUUID(),
-      user_id,  // eslint-disable-line
+      user_id: userId,  // eslint-disable-line
       name,
       description,
       date,
